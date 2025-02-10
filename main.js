136eff2519837039e58dc0b4a122f64fb937a57b
rdalmau23 <rdalmau23@ilg.cat>
2025-02-10 16:47:20 +0100

    feat: select button added

--- main.js
+++ main.js
@@ -1,23 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
     const btnFilter = document.querySelector('.icon-filter');
+    const listaPokemon = document.querySelector(".card-list-pokemon.container");
+    const selectCantidad = document.createElement("select");
+    let URL = "https://pokeapi.co/api/v2/pokemon/";
+    let cantidadPokemon = 10; 
 
-    btnFilter.addEventListener('click', () => {
-        const containerFilter = document.querySelector('.container-filters');
-        containerFilter.classList.toggle('active');
+    selectCantidad.innerHTML = `
+        <option value="10">10</option>
+        <option value="20">20</option>
+        <option value="50">50</option>
+    `;
+    
+    selectCantidad.addEventListener("change", (e) => {
+        cantidadPokemon = parseInt(e.target.value);
+        cargarPokemones();
+    });
+    
+    document.querySelector(".container-filter").appendChild(selectCantidad);
+
+    btnFilter.addEventListener("click", () => {
+        const containerFilter = document.querySelector(".container-filters");
+        containerFilter.classList.toggle("active");
     });
 
-    const listaPokemon = document.querySelector(".card-list-pokemon.container");
-    let URL = "https://pokeapi.co/api/v2/pokemon/";
-
-    for (let i = 1; i <= 151; i++) {
-        fetch(URL + i)
-            .then((response) => response.json())
-            .then(data => mostrarPokemon(data));
+    function cargarPokemones() {
+        listaPokemon.innerHTML = "";
+        for (let i = 1; i <= cantidadPokemon; i++) {
+            fetch(URL + i)
+                .then(response => response.json())
+                .then(data => mostrarPokemon(data));
+        }
     }
 
     function mostrarPokemon(poke) {
-
-        let tipos = poke.types.map((type) => `<p class="${type.type.name} tipo">${type.type.name}</p>`);
+        let tipos = poke.types.map(type => `<p class="${type.type.name} tipo">${type.type.name}</p>`);
         tipos = tipos.join('');
 
         const div = document.createElement("div");
@@ -41,4 +57,7 @@ document.addEventListener("DOMContentLoaded", () => {
         `;
         listaPokemon.append(div);
     }
+
+    cargarPokemones();
 });
+
